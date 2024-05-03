80410cf75d70cb11660217caa0a0884ddc63218f
Märten Mändla <marten.mandla@gmail.com>
2024-05-03 15:57:48 +0300

    Small update in between

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,3 +8,8 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = var.region
+  shared_credentials_files = [ "$HOME/.aws/credentials" ]
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,16 @@
 variable "region" {
     type = string
     description = "Default AWS region"
-    default = "value"
+    default = "eu-north-1"
+}
+
+variable "prefix" {
+  type = string
+  description = "Prefix for all resources"
+  default = "aws-klaus"
+}
+
+variable "cidr_block" {
+  type = string
+  default = "10.0.0.0/16"
 }
