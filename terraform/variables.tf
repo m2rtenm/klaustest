70b96b3ac43f06d4fb76e1f77f9abe8b9ca3014f
Märten Mändla <marten.mandla@gmail.com>
2024-05-04 19:26:26 +0300

    VPC and subnets described

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,3 +14,43 @@ variable "cidr_block" {
   type = string
   default = "10.0.0.0/16"
 }
+
+variable "az_list" {
+  type = list(string)
+  default = [ "eu-north-1a", "eu-north-1b", "eu-north-1c" ]
+}
+
+variable "public_subnet_1_cidr" {
+  type = string
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  type = string
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  type = string
+  default = "10.0.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  type = string
+  default = "10.0.4.0/24"
+}
+
+variable "db_subnet_1_cidr" {
+  type = string
+  default = "10.0.5.0/24"
+}
+
+variable "db_subnet_2_cidr" {
+  type = string
+  default = "10.0.6.0/24"
+}
+
+variable "bastion_subnet_cidr" {
+  type = string
+  default = "10.0.7.0/24"
+}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "app_vpc" {
   cidr_block = var.cidr_block
 
   enable_dns_hostnames = "true"
@@ -9,3 +9,77 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id = aws_vpc.app_vpc.id
+  cidr_block = var.public_subnet_1_cidr
+  map_public_ip_on_launch = true
+  availability_zone = var.az_list[0]
+
+  tags = {
+    "Name" = "${var.prefix}-public_subnet_1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id = aws_vpc.app_vpc.id
+  cidr_block = var.public_subnet_2_cidr
+  map_public_ip_on_launch = true
+  availability_zone = var.az_list[1]
+
+  tags = {
+    "Name" = "${var.prefix}-public_subnet_2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id = aws_vpc.app_vpc.id
+  cidr_block = var.private_subnet_1_cidr
+  map_public_ip_on_launch = false
+  availability_zone = var.az_list[0]
+
+  tags = {
+    "Name" = "${var.prefix}-private_subnet_1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id = aws_vpc.app_vpc.id
+  cidr_block = var.private_subnet_2_cidr
+  map_public_ip_on_launch = false
+  availability_zone = var.az_list[1]
+
+  tags = {
+    "Name" = "${var.prefix}-private_subnet_2"
+  }
+}
+
+resource "aws_subnet" "db_subnet_1" {
+  vpc_id = aws_vpc.app_vpc.id
+  cidr_block = var.db_subnet_1_cidr
+  availability_zone = var.az_list[0]
+
+  tags = {
+    "Name" = "${var.prefix}-db_subnet_1"
+  }
+}
+
+resource "aws_subnet" "db_subnet_2" {
+  vpc_id = aws_vpc.app_vpc.id
+  cidr_block = var.db_subnet_2_cidr
+  availability_zone = var.az_list[1]
+
+  tags = {
+    "Name" = "${var.prefix}-db_subnet_2"
+  }
+}
+
+resource "aws_subnet" "bastion_subnet" {
+  vpc_id = aws_vpc.app_vpc.id
+  cidr_block = var.bastion_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone = var.az_list[0]
+
+  tags = {
+    "Name" = "${var.prefix}-bastion_subnet"
+  }
+}
